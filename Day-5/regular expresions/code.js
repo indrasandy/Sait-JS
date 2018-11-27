fad5d4c60ed953a4eb8c4af1ff6b169950915099
Indrajeet Sandhu <indrajeet.sandhu@edu.sait.ca>
2018-11-27 15:35:30 -0700

    test

--- Day-5/regular expresions/code.js
+++
@@ -1,7 +0,0 @@
-// *******Regular Expresions************
-
-var regExp=new RegExp('abc');
-
-console.log(regExp.test('abceg'));
-console.log(/abc/.test('abdju'));
-console.log(/abc/.test('abcdju'));
